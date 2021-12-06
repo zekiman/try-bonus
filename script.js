fb88c47c6ae604ba5c80a3b6b17e0439036e1cd5
zekiman <87340558+zekiman@users.noreply.github.com>
2021-12-06 17:02:56 +0200

    updating project
    updating project

--- script.js
+++ script.js
@@ -32,21 +32,51 @@ for (i = 0; i < coll.length; i++) {
 }
 
 let codeDiv = document.querySelector('.promo-code-div')
+let goDiv = document.querySelector('.submit-button')
 let copyIcon = document.querySelector('.copy-icon-div').children[0]
+let checkIcon = document.querySelector('.copy-icon-div').children[1]
+let goWinxbetText = document.querySelector('.gowinxbet')
+let arrowicon = document.querySelector('.arrowicon')
+let textBrl = document.querySelector('.brl10')
 
 
 codeDiv.addEventListener('click', function () {
     navigator.clipboard.writeText('10BRL');
-    copyIcon.style.color="green"
+    copyIcon.classList.add('done')
+    console.log(checkIcon)
+    checkIcon.classList.remove('done')
     copyIcon.style.transition = "all 2s";
+    
 })
 
 codeDiv.addEventListener("mouseover",function(){
+    codeDiv.style.backgroundColor="#000";
+    copyIcon.style.color="#ffd300"
+    checkIcon.style.color="#ffd300"
+    textBrl.style.color="#ffd300"
     copyIcon.style.fontSize = '42px';
+    checkIcon.style.fontSize = '42px';
 
 })
 
-codeDiv.addEventListener("mouseleave",function(){
-    copyIcon.style.fontSize = '36px';
+goDiv.addEventListener("mouseover",function(){
+    goDiv.style.backgroundColor="#000";
+    goWinxbetText.style.color="#ffd300";
+    arrowicon.style.color="#ffd300";
+})
 
+codeDiv.addEventListener("mouseleave",function(){
+    codeDiv.style.backgroundColor="#fff"
+    copyIcon.style.fontSize = '36px';
+    checkIcon.style.fontSize = '36px';
+    copyIcon.style.color="#000"
+    checkIcon.style.color="#000"
+    textBrl.style.color="#000"
+})
+
+goDiv.addEventListener("mouseleave",function(){
+    goDiv.style.backgroundColor="#ffd300"
+    goWinxbetText.style.color="#000";
+    arrowicon.style.color="#000";
+    goDiv.style.transition = "all 1s";
 })
